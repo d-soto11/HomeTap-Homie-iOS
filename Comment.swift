cf2aba4147812dc6f850b7eff485ebab8ab9770d
juanestebanchaparromachete <je.chaparro10@gmail.com>
2017-09-08 18:50:36 -0500

    comments 2.0

---
+++ Comment.swift
@@ -0,0 +1,85 @@
+//
+//  Comment.swift
+//  HomeTapHomie
+//
+//  Created by juan esteban chaparro on 8/09/17.
+//  Copyright © 2017 Tres Astronautas. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class Comment: HometapObject {
+
+    public override init(dict: [String : AnyObject]) {
+        super.init(dict: dict)
+        
+        if let body = dict["body"] {
+            self.body = (body as? String)
+        }
+        if let clientID = dict["clientID"] {
+            self.clientID = (clientID as? String)
+        }
+        if let clientName = dict["clientName"] {
+            self.clientName = (clientName as? String)
+        }
+        if let date = dict["date"] {
+            self.date = Date(fromString: (date as! String), withFormat: .Try)
+        }
+        if let homieID = dict["homieID"] {
+            self.homieID = (homieID as? String)
+        }
+        if let rating = dict["rating"] {
+            self.rating = (rating as? Double)
+        }
+        if let serviceID = dict["serviceID"] {
+            self.serviceID = (serviceID as? String)
+        }
+        if let homieName = dict["homieName"] {
+            self.homieName = (homieName as? String)
+        }
+    }
+    
+    class func withID(id: String, callback: @escaping (_ s: Comment?)->Void){
+        K.Database.ref().child("comments").child(id).observe(DataEventType.value, with: { (snapshot) in
+            if let dict = snapshot.value as? [String:AnyObject] {
+                callback(Comment(dict: dict))
+            } else {
+                callback(nil)
+            }
+        })
+    }
+    
+    public func save() {
+        if self.body != nil {
+            original_dictionary["body"] = self.body as AnyObject
+        }
+        if self.clientID != nil {
+            original_dictionary["clientID"] = self.clientID as AnyObject
+        }
+        if self.clientName != nil {
+            original_dictionary["clientName"] = self.clientName as AnyObject
+        }
+        if self.date != nil {
+            original_dictionary["date"] = self.date?.toString(format: .Default) as AnyObject
+        }
+        if self.homieID != nil {
+            original_dictionary["homieID"] = self.homieID as AnyObject
+        }
+        if self.rating != nil {
+            original_dictionary["rating"] = self.rating as AnyObject
+        }
+        super.save(route: "comments")
+    }
+    
+    var body: String?
+    var clientID: String?
+    var clientName: String?
+    var homieName: String?
+    var date: Date?
+    var homieID: String?
+    var rating: Double?
+    var serviceID: String?
+    
+    
+}
--- ViewControllerComents.swift
+++ ViewControllerComents.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 import Cosmos
-class ViewControllerComents: UIViewController {
+import MBProgressHUD
+
+class ViewControllerComents: UIViewController, UITextViewDelegate {
     
     
+    @IBOutlet weak var cosmosView: CosmosView!
     
     var buttonsDescription: [Int] = [0,0,0,0]
+    
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var heightView: NSLayoutConstraint!
     
     @IBOutlet weak var fastButton: UIButton!
     
@@ -32,11 +39,63 @@ class ViewControllerComents: UIViewController {
     
     @IBOutlet weak var mainViewComents: UIView!
     
+    private var compliments: [String:Bool] = [:]
+    
+    var service : Service?
+    
+    var briefService : Service?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.comentText.delegate = self
+        
+        
+        self.heightView.constant = 395
+        self.buttonCallHometap.alpha = 0
+        self.buttonCallHometap.isEnabled = false
+        self.badRatingLable.alpha = 0
+        
+        cosmosView.didFinishTouchingCosmos = { rating in
+            if (rating < 3)
+            {
+                self.showCall()
+                
+            }
+            else{
+                self.hiddeCall()
+                
+            }
+        }
+        
+        cosmosView.didTouchCosmos = { rating in
+            if (rating < 3)
+            {
+                self.showCall()
+                
+            }
+            else{
+                self.hiddeCall()
+                
+            }
+        }
         
         
         // Do any additional setup after loading the view.
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            self.topConstraint.constant = 20
+            UIView.animate(withDuration: 0.4, delay: 0.1, animations: {
+                self.view.layoutIfNeeded()
+                
+                
+            })
+            return false
+        }
+        return true
     }
     
     override func didReceiveMemoryWarning() {
@@ -52,26 +111,30 @@ class ViewControllerComents: UIViewController {
         
         self.fastButton.layer.borderWidth = 1
         self.fastButton.roundCorners(radius: K.UI.coment_round_px)
-        self.fastButton.layer.borderColor = UIColor.gray.cgColor
+        self.fastButton.bordered(color: UIColor.gray)
         
         self.cleanButton.layer.borderWidth = 1
         self.cleanButton.roundCorners(radius: K.UI.coment_round_px)
-        self.cleanButton.layer.borderColor = UIColor.gray.cgColor
+        self.cleanButton.bordered(color: UIColor.gray)
         
         self.friendlyButton.layer.borderWidth = 1
         self.friendlyButton.roundCorners(radius: K.UI.coment_round_px)
-        self.friendlyButton.layer.borderColor = UIColor.gray.cgColor
+        self.friendlyButton.bordered(color: UIColor.gray)
         
         self.onTimeButton.layer.borderWidth = 1
         self.onTimeButton.roundCorners(radius: K.UI.coment_round_px)
-        self.onTimeButton.layer.borderColor = UIColor.gray.cgColor
+        self.onTimeButton.bordered(color: UIColor.gray)
         
         self.doneButton.roundCorners(radius: K.UI.special_round_px)
-        self.buttonCallHometap.roundCorners(radius: K.UI.light_round_px)
-        self.mainViewComents.roundCorners(radius: K.UI.special_round_px)
+        self.buttonCallHometap.roundCorners(radius: K.UI.special_round_px)
+        self.mainViewComents.bordered(color: UIColor.gray)
         
         self.comentText.layer.borderWidth = 1
         self.comentText.layer.borderColor =  UIColor.gray.cgColor
+        self.mainViewComents.roundCorners(radius: K.UI.special_round_px)
+        
+
+        
         
         
     }
@@ -85,17 +148,21 @@ class ViewControllerComents: UIViewController {
         case 0:
             
             if(buttonsDescription[0] == 0){
+                self.fastButton.bordered(color: .clear)
+                fastButton.layer.borderWidth = 1
                 self.fastButton.backgroundColor = K.UI.main_color
-                self.fastButton.titleLabel?.textColor = UIColor.white
+                self.fastButton.setTitleColor(UIColor.white , for: .normal)
                 self.fastButton.roundCorners(radius: K.UI.coment_round_px)
-                self.fastButton.bordered(color: K.UI.main_color)
                 buttonsDescription[0] = 1
             }
             else{
-                self.fastButton.backgroundColor = UIColor.white
-                self.fastButton.titleLabel?.textColor = UIColor.gray
-                self.fastButton.roundCorners(radius: K.UI.coment_round_px)
+                
                 self.fastButton.bordered(color: UIColor.gray)
+                self.fastButton.layer.borderWidth = 1
+                self.fastButton.backgroundColor = UIColor.white
+                self.fastButton.setTitleColor(UIColor.gray , for: .normal)
+                self.fastButton.roundCorners(radius: K.UI.coment_round_px)
+                
                 buttonsDescription[0] = 0
             }
             
@@ -103,36 +170,34 @@ class ViewControllerComents: UIViewController {
             
             if(buttonsDescription[1] == 0){
                 
+                self.onTimeButton.bordered(color: .clear)
                 self.onTimeButton.backgroundColor = K.UI.main_color
-                self.onTimeButton.titleLabel?.textColor = UIColor.white
+                self.onTimeButton.setTitleColor(UIColor.white , for: .normal)
                 self.onTimeButton.roundCorners(radius: K.UI.coment_round_px)
-                self.onTimeButton.bordered(color: K.UI.main_color)
                 buttonsDescription[1] = 1
             }
             else{
-                
-                self.onTimeButton.backgroundColor = UIColor.white
-                self.onTimeButton.titleLabel?.textColor = UIColor.gray
-                self.onTimeButton.roundCorners(radius: K.UI.coment_round_px)
                 self.onTimeButton.bordered(color: UIColor.gray)
+                self.onTimeButton.backgroundColor = UIColor.white
+                self.onTimeButton.setTitleColor(UIColor.gray , for: .normal)
+                self.onTimeButton.roundCorners(radius: K.UI.coment_round_px)
                 buttonsDescription[1] = 0
             }
             
         case 2:
             if(buttonsDescription[2] == 0){
                 
+                self.cleanButton.bordered(color: .clear)
                 self.cleanButton.backgroundColor = K.UI.main_color
-                self.cleanButton.titleLabel?.textColor = UIColor.white
+                self.cleanButton.setTitleColor(UIColor.white , for: .normal)
                 self.cleanButton.roundCorners(radius: K.UI.coment_round_px)
-                self.cleanButton.bordered(color: K.UI.main_color)
                 buttonsDescription[2] = 1
             }
             else{
-                
-                self.cleanButton.backgroundColor = UIColor.white
-                self.cleanButton.titleLabel?.textColor = UIColor.gray
-                self.cleanButton.roundCorners(radius: K.UI.coment_round_px)
                 self.cleanButton.bordered(color: UIColor.gray)
+                self.cleanButton.backgroundColor = UIColor.white
+                self.cleanButton.setTitleColor(UIColor.gray , for: .normal)
+                self.cleanButton.roundCorners(radius: K.UI.coment_round_px)
                 buttonsDescription[2] = 0
             }
             
@@ -140,19 +205,19 @@ class ViewControllerComents: UIViewController {
             
             if(buttonsDescription[3] == 0){
                 
+                self.friendlyButton.bordered(color: .clear)
                 self.friendlyButton.backgroundColor = K.UI.main_color
-                self.friendlyButton.titleLabel?.textColor = UIColor.white
+                self.friendlyButton.setTitleColor(UIColor.white , for: .normal)
                 self.friendlyButton.roundCorners(radius: K.UI.coment_round_px)
-                self.friendlyButton.bordered(color: K.UI.main_color)
                 buttonsDescription[3] = 1
                 
             }
             else{
-                
-                self.friendlyButton.backgroundColor = UIColor.white
-                self.friendlyButton.titleLabel?.textColor = UIColor.gray
-                self.friendlyButton.roundCorners(radius: K.UI.coment_round_px)
+            
                 self.friendlyButton.bordered(color: UIColor.gray)
+                self.friendlyButton.backgroundColor = UIColor.white
+                self.friendlyButton.setTitleColor(UIColor.gray , for: .normal)
+                self.friendlyButton.roundCorners(radius: K.UI.coment_round_px)
                 buttonsDescription[3] = 0
             }
         default:
@@ -163,16 +228,101 @@ class ViewControllerComents: UIViewController {
     }
     
     
+    func showCall() {
+        
+        self.heightView.constant = 486
+    
+        UIView.animate(withDuration: 0.8, delay: 0.2, animations: {
+            self.view.layoutIfNeeded()
+            
+            self.buttonCallHometap.alpha = 1
+            self.buttonCallHometap.isEnabled = true
+            self.badRatingLable.alpha = 1
+           
+            
+            
+        })
+        
+        
+        // Save rating here
+    }
+    func hiddeCall() {
+        
+
+        self.heightView.constant = 395
+        
+        UIView.animate(withDuration: 0.8, delay: 0.2, animations: {
+            self.view.layoutIfNeeded()
+            
+            self.buttonCallHometap.alpha = 0
+            self.buttonCallHometap.isEnabled = false
+            self.badRatingLable.alpha = 0
+            
+            
+            
+        })
+    }
     
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    @IBAction func submitinfomation(_ sender: Any) {
+       
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        for i in 0...3 {
+            
+            if (buttonsDescription[i]==1)
+            {
+                self.compliments["\(3)"] = true
+            }
+        }
+        self.compliments["\(3)"] = true
+        let comment = Comment(dict: [:])
+        comment.body = self.comentText.text
+        comment.homieID = K.User.homie?.uid
+        comment.homieName = K.User.homie?.name
+        comment.date = Date()
+        let _ = service?.client{ (client) in
+            if client != nil {
+                comment.clientID = client?.uid
+                comment.clientName = client?.name
+                comment.rating = self.cosmosView.rating
+                comment.original_dictionary["tags"] = self.compliments as AnyObject
+                comment.original_dictionary["tipo"] = 1 as AnyObject
+                comment.save()
+                MBProgressHUD.hide(for: self.view, animated: true)
+            }
+        }
+        
+        
+        
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "ViewControllerUsedInventoryMaterial") as! ViewControllerUsedInventoryMaterial
+        self.present(controller, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    @IBAction func callHomeTap(_ sender: Any) {
+        
+        let url = URL(string: "tel://3100000000")
+        UIApplication.shared.open(url! , options: [:] , completionHandler: nil)
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        
+        self.topConstraint.constant = -70
+        UIView.animate(withDuration: 0.4, delay: 0.1, animations: {
+            self.view.layoutIfNeeded()
+            
+            
+        })
+        
+        
+    }
+    
+    
+    
     
 }
